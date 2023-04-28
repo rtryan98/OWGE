6c28e71c165000e539570f318ee5a48b9242dca6
Robert <33040161+rtryan98@users.noreply.github.com>
2023-04-29 01:18:46 +0200

    [tech_demo] Early tests.

---
+++ owge_tech_demo/owge_tech_demo/main.cpp
@@ -0,0 +1,19 @@
+#include <cstdint>
+#include <owge_d3d12_base/d3d12_util.hpp>
+#include <owge_render_engine/render_engine.hpp>
+#include <owge_window/window.hpp>
+
+int32_t main()
+{
+    owge::Window window = {};
+    std::unique_ptr<owge::Render_Engine> render_engine;
+    owge::D3D12_Context_Settings d3d12_settings = {
+        .enable_validation = true,
+        .enable_gpu_based_validation = false,
+        .disable_tdr = false,
+        .d3d_feature_level = D3D_FEATURE_LEVEL_12_2,
+        .static_samplers = {}
+    };
+    render_engine = std::make_unique<owge::Render_Engine>(window, d3d12_settings);
+    return 0;
+}
