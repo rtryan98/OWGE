586127751f6bdd6f25064372964005d824aff651
Robert <33040161+rtryan98@users.noreply.github.com>
2023-06-13 18:02:41 +0200

    [tech_demo] Initial testing of the render engine.

--- owge_tech_demo/owge_tech_demo/main.cpp
+++ owge_tech_demo/owge_tech_demo/main.cpp
@@ -1,6 +1,7 @@
 #include <cstdint>
 #include <owge_d3d12_base/d3d12_util.hpp>
 #include <owge_render_engine/render_engine.hpp>
+#include <owge_render_engine/render_procedure/swapchain_pass.hpp>
 #include <owge_window/window.hpp>
 
 int32_t main()
@@ -17,7 +18,7 @@ int32_t main()
         .enable_validation = true,
         .enable_gpu_based_validation = false,
         .disable_tdr = false,
-        .d3d_feature_level = D3D_FEATURE_LEVEL_12_2,
+        .d3d_feature_level = D3D_FEATURE_LEVEL_12_1,
         .static_samplers = {}
     };
     owge::Render_Engine_Settings render_engine_settings = {
@@ -28,9 +29,18 @@ int32_t main()
         window->get_hwnd(),
         d3d12_settings,
         render_engine_settings);
+    owge::Swapchain_Pass_Settings swapchain_pass_settings = {
+        .clear_color = { 0.1250f, 0.0f, 0.0f, 0.0f }
+    };
+    auto swapchain_pass = std::make_unique<owge::Swapchain_Pass>(
+        swapchain_pass_settings);
+
+    render_engine->add_procedure(swapchain_pass.get());
+
     while (window->get_data().alive)
     {
         window->poll_events();
+        render_engine->render();
     }
     return 0;
 }
