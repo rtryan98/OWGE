f16afad6f3e585633dea244bc0f1a9dec395e01c
Robert <33040161+rtryan98@users.noreply.github.com>
2023-06-13 18:00:49 +0200

    [render_engine] Changes to render_procedure. Use payload instead of cmd only and give it a name.

--- owge_render_engine/owge_render_engine/render_procedure/render_procedure.hpp
+++ owge_render_engine/owge_render_engine/render_procedure/render_procedure.hpp
@@ -1,14 +1,36 @@
 #pragma once
 
 #include <d3d12.h>
+#include <string>
 
 namespace owge
 {
+class Render_Engine;
+class Barrier_Builder;
+class D3D12_Swapchain;
+
+struct Render_Procedure_Payload
+{
+    Render_Engine* render_engine;
+    ID3D12GraphicsCommandList9* cmd;
+    Barrier_Builder* barrier_builder;
+    D3D12_Swapchain* swapchain;
+};
+
 class Render_Procedure
 {
 public:
     virtual ~Render_Procedure() = default;
 
-    virtual void process(ID3D12GraphicsCommandList9* cmd) = 0;
+    virtual void process(const Render_Procedure_Payload& payload) = 0;
+
+protected:
+    Render_Procedure() = default;
+    Render_Procedure(std::string_view name)
+        : m_name( name )
+    {}
+
+private:
+    std::string m_name;
 };
 }
