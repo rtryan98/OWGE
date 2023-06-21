80190dea518301eae6fdc560dce2ec4048170732
Robert <33040161+rtryan98@users.noreply.github.com>
2023-06-21 22:34:41 +0200

    [render_engine] Command List abstraction for ease of use.

---
+++ owge_render_engine/owge_render_engine/command_list.cpp
@@ -0,0 +1,258 @@
+#include "owge_render_engine/command_list.hpp"
+#include "owge_render_engine/render_engine.hpp"
+#include "owge_render_engine/bindless.hpp"
+
+#include <owge_d3d12_base/d3d12_swapchain.hpp>
+
+#if OWGE_USE_WIN_PIX_EVENT_RUNTIME
+#include <WinPixEventRuntime/pix3.h>
+#endif // OWGE_USE_WIN_PIX_EVENT_RUNTIME
+
+#include <array>
+
+namespace owge
+{
+Barrier_Builder::Barrier_Builder(Render_Engine* render_engine, ID3D12GraphicsCommandList7* cmd)
+    : m_render_engine(render_engine), m_cmd(cmd)
+{}
+
+void Barrier_Builder::push(const Texture_Barrier& barrier)
+{
+    if (barrier.texture.is_null_handle())
+    {
+        auto swapchain_resources = barrier.swapchain->get_acquired_resources();
+        m_texture_barriers.push_back({
+            .SyncBefore = barrier.sync_before,
+            .SyncAfter = barrier.sync_after,
+            .AccessBefore = barrier.access_before,
+            .AccessAfter = barrier.access_after,
+            .LayoutBefore = barrier.layout_before,
+            .LayoutAfter = barrier.layout_after,
+            .pResource = swapchain_resources.buffer,
+            .Subresources = barrier.subresources,
+            .Flags = barrier.flags
+            });
+    }
+    else
+    {
+        auto& texture = m_render_engine->get_texture(barrier.texture);
+        m_texture_barriers.push_back({
+            .SyncBefore = barrier.sync_before,
+            .SyncAfter = barrier.sync_after,
+            .AccessBefore = barrier.access_before,
+            .AccessAfter = barrier.access_after,
+            .LayoutBefore = barrier.layout_before,
+            .LayoutAfter = barrier.layout_after,
+            .pResource = texture.resource,
+            .Subresources = barrier.subresources,
+            .Flags = barrier.flags
+            });
+    }
+}
+
+void Barrier_Builder::push(const Buffer_Barrier& barrier)
+{
+    auto& buffer = m_render_engine->get_buffer(barrier.buffer);
+    m_buffer_barriers.push_back({
+        .SyncBefore = barrier.sync_before,
+        .SyncAfter = barrier.sync_after,
+        .AccessBefore = barrier.access_before,
+        .AccessAfter = barrier.access_after,
+        .pResource = buffer.resource,
+        .Offset = 0,
+        .Size = ~0ull
+        });
+}
+
+void Barrier_Builder::push(const Memory_Barrier& barrier)
+{
+    m_global_barriers.push_back({
+        .SyncBefore = barrier.sync_before,
+        .SyncAfter = barrier.sync_after,
+        .AccessBefore = barrier.access_before,
+        .AccessAfter = barrier.access_after
+        });
+}
+
+void Barrier_Builder::flush()
+{
+    uint32_t barrier_group_count = 0;
+    std::array<D3D12_BARRIER_GROUP, 3> barrier_groups = {};
+    if (m_texture_barriers.size() > 0)
+    {
+        auto& barrier_group = barrier_groups[barrier_group_count];
+        barrier_group.Type = D3D12_BARRIER_TYPE_TEXTURE;
+        barrier_group.NumBarriers = uint32_t(m_texture_barriers.size());
+        barrier_group.pTextureBarriers = m_texture_barriers.data();
+        barrier_group_count += 1;
+    }
+    if (m_buffer_barriers.size() > 0)
+    {
+        auto& barrier_group = barrier_groups[barrier_group_count];
+        barrier_group.Type = D3D12_BARRIER_TYPE_BUFFER;
+        barrier_group.NumBarriers = uint32_t(m_buffer_barriers.size());
+        barrier_group.pBufferBarriers = m_buffer_barriers.data();
+        barrier_group_count += 1;
+    }
+    if (m_global_barriers.size() > 0)
+    {
+        auto& barrier_group = barrier_groups[barrier_group_count];
+        barrier_group.Type = D3D12_BARRIER_TYPE_GLOBAL;
+        barrier_group.NumBarriers = uint32_t(m_global_barriers.size());
+        barrier_group.pGlobalBarriers = m_global_barriers.data();
+        barrier_group_count += 1;
+    }
+    m_cmd->Barrier(barrier_group_count, barrier_groups.data());
+    m_texture_barriers.clear();
+    m_buffer_barriers.clear();
+    m_global_barriers.clear();
+}
+
+Command_List::Command_List(Render_Engine* render_engine, ID3D12GraphicsCommandList7* cmd)
+    : m_render_engine(render_engine), m_cmd(cmd)
+{}
+
+Barrier_Builder Command_List::acquire_barrier_builder()
+{
+    return Barrier_Builder(m_render_engine, m_cmd);
+}
+
+void Command_List::clear_depth_stencil(Texture_Handle texture, D3D12_CLEAR_FLAGS flags, float depth, uint8_t stencil)
+{
+    m_cmd->ClearDepthStencilView(m_render_engine->get_cpu_descriptor_from_texture(texture), flags, depth, stencil, 0, nullptr);
+}
+
+void Command_List::clear_render_target(D3D12_Swapchain* swapchain, float clear_color[4])
+{
+    auto swapchain_resources = swapchain->get_acquired_resources();
+    m_cmd->ClearRenderTargetView(swapchain_resources.rtv_descriptor, clear_color, 0, nullptr);
+}
+
+void Command_List::clear_render_target(Texture_Handle texture, float clear_color[4])
+{
+    m_cmd->ClearRenderTargetView(m_render_engine->get_cpu_descriptor_from_texture(texture), clear_color, 0, nullptr);
+}
+
+void Command_List::dispatch(uint32_t x, uint32_t y, uint32_t z)
+{
+    m_cmd->Dispatch(x, y, z);
+}
+
+void Command_List::dispatch_div_by_workgroups(Pipeline_Handle pso, uint32_t x, uint32_t y, uint32_t z)
+{
+    auto& pipeline = m_render_engine->get_pipeline(pso);
+    m_cmd->Dispatch(x / pipeline.workgroups_x, y / pipeline.workgroups_y, z / pipeline.workgroups_z);
+}
+
+void Command_List::dispatch_mesh(uint32_t x, uint32_t y, uint32_t z)
+{
+    m_cmd->DispatchMesh(x, y, z);
+}
+
+void Command_List::dispatch_mesh_div_by_workgroups(Pipeline_Handle pso, uint32_t x, uint32_t y, uint32_t z)
+{
+    auto& pipeline = m_render_engine->get_pipeline(pso);
+    m_cmd->DispatchMesh(x / pipeline.workgroups_x, y / pipeline.workgroups_y, z / pipeline.workgroups_z);
+}
+
+void Command_List::draw(uint32_t vertex_count, uint32_t vertex_offset,
+    uint32_t instance_count, uint32_t instance_offset)
+{
+    m_cmd->DrawInstanced(vertex_count, instance_count, vertex_offset, instance_offset);
+}
+
+void Command_List::draw_indexed(uint32_t index_count, uint32_t index_offset,
+    uint32_t instance_count, uint32_t instance_offset, uint32_t base_vertex)
+{
+    m_cmd->DrawIndexedInstanced(index_count, instance_count, index_offset, base_vertex, instance_offset);
+}
+
+void Command_List::set_bindset_compute(const Bindset& bindset)
+{
+    auto& alloc = bindset.allocation;
+    m_cmd->SetComputeRoot32BitConstants(0, 2, &alloc, 0);
+}
+
+void Command_List::set_bindset_graphics(const Bindset& bindset)
+{
+    auto& alloc = bindset.allocation;
+    m_cmd->SetGraphicsRoot32BitConstants(0, 2, &alloc, 0);
+}
+
+void Command_List::set_pipeline_state(Pipeline_Handle handle)
+{
+    auto& pipeline = m_render_engine->get_pipeline(handle);
+    m_cmd->SetPipelineState(pipeline.pso);
+}
+
+void Command_List::set_render_targets(std::span<Texture_Handle> textures, Texture_Handle depth_stencil)
+{
+    if (!depth_stencil.is_null_handle())
+    {
+        uint32_t render_target_count = 0;
+        std::array<D3D12_CPU_DESCRIPTOR_HANDLE, 8> render_targets;
+        for (auto texture : textures)
+        {
+            render_targets[render_target_count] = m_render_engine->get_cpu_descriptor_from_texture(texture);
+            render_target_count += 1;
+        }
+        D3D12_CPU_DESCRIPTOR_HANDLE ds_cpu_handle = m_render_engine->get_cpu_descriptor_from_texture(depth_stencil);
+        m_cmd->OMSetRenderTargets(render_target_count, render_targets.data(), false, &ds_cpu_handle);
+    }
+    else
+    {
+        uint32_t render_target_count = 0;
+        std::array<D3D12_CPU_DESCRIPTOR_HANDLE, 8> render_targets;
+        for (auto texture : textures)
+        {
+            render_targets[render_target_count] = m_render_engine->get_cpu_descriptor_from_texture(texture);
+            render_target_count += 1;
+        }
+        m_cmd->OMSetRenderTargets(render_target_count, render_targets.data(), false, nullptr);
+    }
+}
+
+void Command_List::set_render_target_swapchain(D3D12_Swapchain* swapchain, Texture_Handle depth_stencil)
+{
+    if (!depth_stencil.is_null_handle())
+    {
+        auto swapchain_cpu_handle = swapchain->get_acquired_resources().rtv_descriptor;
+        D3D12_CPU_DESCRIPTOR_HANDLE ds_cpu_handle = m_render_engine->get_cpu_descriptor_from_texture(depth_stencil);
+        m_cmd->OMSetRenderTargets(1, &swapchain_cpu_handle, 0, &ds_cpu_handle);
+    }
+    else
+    {
+        auto swapchain_cpu_handle = swapchain->get_acquired_resources().rtv_descriptor;
+        m_cmd->OMSetRenderTargets(1, &swapchain_cpu_handle, 0, nullptr);
+    }
+}
+
+void Command_List::begin_event(
+    [[maybe_unused]] uint8_t red,
+    [[maybe_unused]] uint8_t green,
+    [[maybe_unused]] uint8_t blue,
+    [[maybe_unused]] const char* message)
+{
+#if OWGE_USE_WIN_PIX_EVENT_RUNTIME
+    PIXBeginEvent(m_cmd, PIX_COLOR(red, green, blue), message);
+#endif // OWGE_USE_WIN_PIX_EVENT_RUNTIME
+}
+
+void Command_List::end_event()
+{
+#if OWGE_USE_WIN_PIX_EVENT_RUNTIME
+    PIXEndEvent(m_cmd);
+#endif // OWGE_USE_WIN_PIX_EVENT_RUNTIME
+}
+
+void Command_List::set_marker(
+    [[maybe_unused]] uint8_t red,
+    [[maybe_unused]] uint8_t green,
+    [[maybe_unused]] uint8_t blue,
+    [[maybe_unused]] const char* message)
+{
+#if OWGE_USE_WIN_PIX_EVENT_RUNTIME
+    PIXSetMarker(m_cmd, PIX_COLOR(red, green, blue), message);
+#endif // OWGE_USE_WIN_PIX_EVENT_RUNTIME
+}
+}
---
+++ owge_render_engine/owge_render_engine/command_list.hpp
@@ -0,0 +1,97 @@
+#pragma once
+
+#include "owge_render_engine/resource.hpp"
+
+#include <cstdint>
+#include <d3d12.h>
+#include <span>
+#include <vector>
+
+namespace owge
+{
+class Render_Engine;
+class D3D12_Swapchain;
+struct Bindset;
+
+struct Texture_Barrier
+{
+    Texture_Handle texture;
+    D3D12_Swapchain* swapchain;
+    D3D12_BARRIER_SYNC sync_before;
+    D3D12_BARRIER_SYNC sync_after;
+    D3D12_BARRIER_ACCESS access_before;
+    D3D12_BARRIER_ACCESS access_after;
+    D3D12_BARRIER_LAYOUT layout_before;
+    D3D12_BARRIER_LAYOUT layout_after;
+    D3D12_BARRIER_SUBRESOURCE_RANGE subresources;
+    D3D12_TEXTURE_BARRIER_FLAGS flags;
+};
+
+struct Buffer_Barrier
+{
+    Buffer_Handle buffer;
+    D3D12_BARRIER_SYNC sync_before;
+    D3D12_BARRIER_SYNC sync_after;
+    D3D12_BARRIER_ACCESS access_before;
+    D3D12_BARRIER_ACCESS access_after;
+};
+
+struct Memory_Barrier
+{
+    D3D12_BARRIER_SYNC sync_before;
+    D3D12_BARRIER_SYNC sync_after;
+    D3D12_BARRIER_ACCESS access_before;
+    D3D12_BARRIER_ACCESS access_after;
+};
+
+class Barrier_Builder
+{
+public:
+    Barrier_Builder(Render_Engine* render_engine, ID3D12GraphicsCommandList7* cmd);
+
+    void push(const Texture_Barrier& barrier);
+    void push(const Buffer_Barrier& barrier);
+    void push(const Memory_Barrier& barrier);
+    void flush();
+
+private:
+    ID3D12GraphicsCommandList7* m_cmd;
+    Render_Engine* m_render_engine;
+    std::vector<D3D12_TEXTURE_BARRIER> m_texture_barriers;
+    std::vector<D3D12_BUFFER_BARRIER> m_buffer_barriers;
+    std::vector<D3D12_GLOBAL_BARRIER> m_global_barriers;
+};
+
+class Command_List
+{
+public:
+    Command_List(Render_Engine* render_engine, ID3D12GraphicsCommandList7* cmd);
+
+    [[nodiscard]] Barrier_Builder acquire_barrier_builder();
+
+    void clear_depth_stencil(Texture_Handle texture, D3D12_CLEAR_FLAGS flags, float depth, uint8_t stencil);
+    void clear_render_target(D3D12_Swapchain* swapchain, float clear_color[4]);
+    void clear_render_target(Texture_Handle texture, float clear_color[4]);
+    void dispatch(uint32_t x, uint32_t y, uint32_t z);
+    void dispatch_div_by_workgroups(Pipeline_Handle pso, uint32_t x, uint32_t y, uint32_t z);
+    void dispatch_mesh(uint32_t x, uint32_t y, uint32_t z);
+    void dispatch_mesh_div_by_workgroups(Pipeline_Handle pso, uint32_t x, uint32_t y, uint32_t z);
+    void draw(uint32_t vertex_count, uint32_t vertex_offset,
+        uint32_t instance_count, uint32_t instance_offset);
+    void draw_indexed(uint32_t index_count, uint32_t index_offset,
+        uint32_t instance_count, uint32_t instance_offset, uint32_t base_vertex);
+    void set_bindset_compute(const Bindset& bindset);
+    void set_bindset_graphics(const Bindset& bindset);
+    void set_pipeline_state(Pipeline_Handle handle);
+    void set_render_targets(std::span<Texture_Handle> textures, Texture_Handle depth_stencil);
+    void set_render_target_swapchain(D3D12_Swapchain* swapchain, Texture_Handle depth_stencil);
+
+    void begin_event(uint8_t red, uint8_t green, uint8_t blue, const char* message);
+    void end_event();
+    void set_marker(uint8_t red, uint8_t green, uint8_t blue, const char* message);
+
+private:
+    Render_Engine* m_render_engine;
+    ID3D12GraphicsCommandList7* m_cmd;
+};
+}
