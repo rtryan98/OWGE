65b8d25f80a13e8661575d943fc7d6c4a454a231
Robert <33040161+rtryan98@users.noreply.github.com>
2023-07-10 12:12:44 +0200

    [ocean] shift position in developed spectrum.

--- owge_shaders/owge_shaders/ocean/developed_spectrum.cs.hlsl
+++ owge_shaders/owge_shaders/ocean/developed_spectrum.cs.hlsl
@@ -27,20 +27,20 @@ void cs_main(uint3 id : SV_DispatchThreadID)
     float4 spectrum_wavenumber = bnd.spectrum_tex.load_2d<float4>(id.xy);
     float2 spectrum = spectrum_wavenumber.xy;
     float2 wavenumber = spectrum_wavenumber.zw;
-    float mag = max(0.000001, length(wavenumber));
-    float2 spectrum_minus_k = complex_conjugate((bnd.spectrum_tex.load_2d<float4>(uint2(bnd.size, bnd.size) - id.xy) % bnd.size).xy);
+    float mag = max(0.0001, length(wavenumber));
+    float2 spectrum_minus_k = complex_conjugate((bnd.spectrum_tex.load_2d<float2>(uint2(bnd.size, bnd.size) - id.xy) % bnd.size));
     float omega_k = bnd.angular_frequency_tex.load_2d<float>(id.xy);
 
-    float phi = bnd.time * omega_k; // omega(k) * t in [Tessendorf 2001]
+    float phi = bnd.time * omega_k;
     float2 cmul_term = complex_from_polar(1.0, phi);
-    float2 developed_spectrum = 0.5 * (complex_mul(spectrum, cmul_term) + complex_mul(spectrum_minus_k, float2(cmul_term.x, -cmul_term.y)));
+    float2 developed_spectrum = 0.5 * (complex_mul(spectrum, cmul_term) + complex_mul(spectrum_minus_k, complex_conjugate(cmul_term)));
     float2 rotated_developed_spectrum = float2(-developed_spectrum.y, developed_spectrum.x); // multiplication by i
 
     float2 displacement_x = rotated_developed_spectrum * wavenumber.x * (1.0 / mag);
     float2 displacement_y = rotated_developed_spectrum * wavenumber.y * (1.0 / mag);
     float2 displacement_z = developed_spectrum;
 
-    float2 displacement_x_dx;
+    float2 displacement_x_dx = developed_spectrum;
     float2 displacement_y_dx;
     float2 displacement_z_dx;
 
@@ -55,5 +55,8 @@ void cs_main(uint3 id : SV_DispatchThreadID)
     float2 packed_spectrum_y_dx_z_dx = displacement_y_dx + float2(-displacement_z_dx.y, displacement_z_dx.x);
     float2 packed_spectrum_y_dy_z_dy = displacement_y_dy + float2(-displacement_z_dy.y, displacement_z_dy.x);
 
-    bnd.developed_spectrum_tex.store_2d<float2>(id.xy, packed_spectrum_x_y);
+    uint2 shifted_pos = (id.xy + uint2(bnd.size, bnd.size) / 2) % bnd.size;
+
+    //bnd.developed_spectrum_tex.store_2d<float2>(id.xy, packed_spectrum_z_x_dx);
+    bnd.developed_spectrum_tex.store_2d<float2>(shifted_pos, developed_spectrum);
 }
