fac826ba05d13cb909f3ce7f74273b36ca001800
Robert <33040161+rtryan98@users.noreply.github.com>
2023-07-31 10:11:25 +0200

    [ocean] Render the plane.

--- owge_render_techniques/owge_render_techniques/ocean/ocean_surface_render_procedure.cpp
+++ owge_render_techniques/owge_render_techniques/ocean/ocean_surface_render_procedure.cpp
@@ -1,21 +1,40 @@
 #include "owge_render_techniques/ocean/ocean_surface_render_procedure.hpp"
 #include "owge_render_techniques/ocean/ocean_render_resources.hpp"
 
+#include <owge_render_engine/render_engine.hpp>
 #include <owge_render_engine/command_list.hpp>
+#include <owge_render_engine/camera.hpp>
 
 namespace owge
 {
-Ocean_Surface_Render_Procedure::Ocean_Surface_Render_Procedure(Ocean_Settings* settings, Ocean_Simulation_Render_Resources* resources)
+Ocean_Surface_Render_Procedure::Ocean_Surface_Render_Procedure(
+    Ocean_Settings* settings,
+    Ocean_Simulation_Render_Resources* resources,
+    Camera_Data* camera)
     : Render_Procedure("Ocean_Surface_Render")
     , m_settings(settings)
     , m_resources(resources)
+    , m_camera(camera)
 {}
 
 void Ocean_Surface_Render_Procedure::process(const Render_Procedure_Payload& payload)
 {
+    Ocean_Surface_VS_Render_Data vs_render_data = {
+        .view_proj = m_camera->view_proj
+    };
+    payload.render_engine->copy_and_upload_data(
+        sizeof(Ocean_Surface_VS_Render_Data),
+        0,
+        m_resources->ocean_surface_vs_render_data_buffer,
+        0,
+        &vs_render_data);
+
     payload.cmd->set_pipeline_state(m_resources->surface_plane_pso);
+    payload.cmd->set_index_buffer(m_resources->ocean_surface_index_buffer, Index_Type::Uint32);
+    payload.cmd->set_primitive_topology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
     payload.cmd->set_bindset_graphics(m_resources->surface_render_vs_bindset, 0);
     payload.cmd->set_bindset_graphics(m_resources->surface_render_ps_bindset, 2);
 
+    payload.cmd->draw_indexed(m_resources->ocean_surface_index_count, 0, 1, 0, 0);
 }
 }
--- owge_render_techniques/owge_render_techniques/ocean/ocean_surface_render_procedure.hpp
+++ owge_render_techniques/owge_render_techniques/ocean/ocean_surface_render_procedure.hpp
@@ -6,17 +6,21 @@ namespace owge
 {
 struct Ocean_Settings;
 struct Ocean_Simulation_Render_Resources;
+struct Camera_Data;
 
 class Ocean_Surface_Render_Procedure : public Render_Procedure
 {
 public:
     Ocean_Surface_Render_Procedure(
-        Ocean_Settings* settings, Ocean_Simulation_Render_Resources* resources);
+        Ocean_Settings* settings,
+        Ocean_Simulation_Render_Resources* resources,
+        Camera_Data* camera_data);
 
     virtual void process(const Render_Procedure_Payload& payload) override;
 
 private:
     Ocean_Settings* m_settings;
     Ocean_Simulation_Render_Resources* m_resources;
+    Camera_Data* m_camera;
 };
 }
