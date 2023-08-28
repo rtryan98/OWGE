90e8666810120f0e2f1a43b3c98312fe08d8fb23
Robert <33040161+rtryan98@users.noreply.github.com>
2023-08-28 06:59:41 +0200

    [ocean] Switch spectrum used to reduce variables to find bug in normals calculating. Limit spectrum according to Shannon-Nyquist Sampling Theorem.

--- owge_shaders/owge_shaders/ocean/initial_spectrum.cs.hlsl
+++ owge_shaders/owge_shaders/ocean/initial_spectrum.cs.hlsl
@@ -13,10 +13,10 @@ struct Spectrum_Parameters
 struct Ocean_Parameters
 {
     uint  size;                         // N = M = 2^n
-    float lengthscale;                  // L
+    float lengthscales[4];              // L
     float gravity;                      // g
     float ocean_depth;                  // h
-    Spectrum_Parameters spectra[2];
+    Spectrum_Parameters spectra[2];     // Local and Swell
 };
 
 struct Bindset
@@ -42,12 +42,12 @@ void cs_main(uint3 id : SV_DispatchThreadID)
     Ocean_Parameters pars = bnd.params.load_uniform<Ocean_Parameters>();
 
     int2 id_shifted = int2(id.xy) - int2(pars.size, pars.size) / 2;
-    float delta_k = (2.0f * MC_PI) / pars.lengthscale;
+    float delta_k = (2.0f * MC_PI) / pars.lengthscales[id.z];
     float2 k = id_shifted * delta_k;
-    float k_len = length(k);
+    float wavenumber = length(k);
 
-    float omega = oceanography_dispersion_capillary(k_len, pars.gravity, pars.ocean_depth);
-    float omega_d_dk = oceanography_dispersion_capillary_d_dk(k_len, pars.gravity, pars.ocean_depth);
+    float omega = oceanography_dispersion_capillary(wavenumber, pars.gravity, pars.ocean_depth);
+    float omega_d_dk = oceanography_dispersion_capillary_d_dk(wavenumber, pars.gravity, pars.ocean_depth);
     float omega_peak = oceanography_jonswap_omega_peak(pars.gravity, pars.spectra[0].wind_speed, pars.spectra[0].fetch);
     float theta = atan2(k.y, k.x);
 
@@ -55,15 +55,32 @@ void cs_main(uint3 id : SV_DispatchThreadID)
         omega, omega_peak, pars.spectra[0].wind_speed, pars.gravity, pars.spectra[0].fetch, pars.ocean_depth);
     float directional_spectrum = oceanography_donelan_banner_directional_spreading(
         omega, omega_peak, theta);
-    float spectrum = non_directional_spectrum * directional_spectrum;
-    spectrum = sqrt(2.0f * spectrum * abs(omega_d_dk / k_len) * delta_k * delta_k);
-    float2 final_spectrum = box_muller_22(float2(pcg2d(id.xy)) * (1.0/float(0xffffffffu)), 0.0, 1.0) * spectrum;
 
-    if((id_shifted.x == 0 && id_shifted.y == 0))
+    omega_peak = oceanography_pierson_moskowitz_omega_peak(pars.gravity, pars.spectra[0].wind_speed);
+    non_directional_spectrum = oceanography_pierson_moskowitz_spectrum(omega, omega_peak, pars.gravity, pars.spectra[0].wind_speed);
+
+    float spectrum = non_directional_spectrum * directional_spectrum;
+    spectrum = sqrt(2.0f * spectrum * abs(omega_d_dk / wavenumber) * delta_k * delta_k);
+    float2 noise = 1.0 / sqrt(2.0) * float2(
+        box_muller_12(1.0/float(0xffffffffu) * float2(pcg3d(id.xyz).xy)),
+        box_muller_12(1.0/float(0xffffffffu) * float2(pcg3d(id.xyz + uint3(0, 0, 4)).xy)));
+    float2 final_spectrum = noise * spectrum;
+
+    bool sampling_limit = false;
+    float k_min = sqrt(2.0) * 2.0 * MC_PI / pars.lengthscales[id.z];
+    float k_max = MC_PI * pars.size / pars.lengthscales[id.z];
+    if((wavenumber < k_min) || (wavenumber > k_max))
+    {
+        sampling_limit = true;
+    }
+    if ( (id_shifted.x == 0 && id_shifted.y == 0) || deny )
     {
         // We need to set the 0th wavevector to 0.
         // This does not break the calculation either, as this only corresponds to the DC-part of the spectrum.
+        // We cannot sample all wavelengths without loss of information according to the Shannon-Nyquist sampling theorem,
+        // so we need to cut off the spectrum at those wavelengths as well.
         final_spectrum = float2(0.0, 0.0);
+        omega = 0.0;
     }
 
     bnd.spectrum_tex.store_2d_array(id.xyz, float4(final_spectrum, k));
